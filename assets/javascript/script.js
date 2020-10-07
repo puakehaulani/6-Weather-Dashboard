ce0c3b0773310d443784969e12568a7482a1c075
puakehaulani <puakehaulani@gmail.com>
2020-10-07 15:14:26 -0700

    build first url function working, on click entered

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,40 @@
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// apiKey = "d3c8c525fef446aa8fb30692c753cdf6";
 // global variables
-let apiKey = "&appid=d3c8c525fef446aa8fb30692c753cdf6";
-let qCity = "";
-let getCoord = "api.openweathermap.org/data/2.5/weather?q=" + qCity + apiKey;
-let lat = "";
-let lon = "";
-let currentWeather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + apiKey;
-let fiveDay = currentWeather + "exclude=current,minutely,hourly,alerts";
 
-//click function enters user city name into api link
+// build URL
+function buildGetCoord() {
+  let getCoord = "https://api.openweathermap.org/data/2.5/weather?";
+  let coordParams = {
+    "q": $("#cityName").val(),
+    "appid": "d3c8c525fef446aa8fb30692c753cdf6"
+  }
+  console.log(getCoord + $.param(coordParams));
+  return getCoord + $.param(coordParams);
+  //parse to get coords
+}
+
+function buildOneCall() {
+
+
+}
+
+//click functions
+// first call, city name gets coords
+$("#searchBtn").click(function (event) {
+  event.preventDefault();
+  let getCoord = buildGetCoord();
+  console.log(getCoord);
+  $.ajax({
+    url: getCoord,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+  })
+
+  // $(".form-control").val("");
+
+});
 //use response to get lat and lon
 
 //run api call with lat and lon to get current weather and display
