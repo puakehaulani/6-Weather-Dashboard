b8e35625e06e427bbcd44f647bf75204c4c92d7a
puakehaulani <puakehaulani@gmail.com>
2020-10-07 18:50:55 -0700

    refined pseudocode now that the calls are working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,8 @@
-// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-// apiKey = "d3c8c525fef446aa8fb30692c753cdf6";
-// global variables
+// ----global variables----
 
-// build URL
+// ----build URLs----
+// build url to call that enters user's city name and returns response with lat and lon coordinates
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 function buildGetCoord() {
   let getCoord = "https://api.openweathermap.org/data/2.5/weather?";
   let coordParams = {
@@ -11,35 +11,58 @@ function buildGetCoord() {
   }
   console.log(getCoord + $.param(coordParams));
   return getCoord + $.param(coordParams);
-  //parse to get coords
 }
 
-function buildOneCall() {
-
-
+// build url to one call for data to be used for display
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+function buildOneCall(latvar, lonvar) {
+  let oneCall = "https://api.openweathermap.org/data/2.5/onecall?";
+  let oneCallParams = {
+    "lat": latvar,
+    "lon": lonvar,
+    "appid": "d3c8c525fef446aa8fb30692c753cdf6"
+  }
+  console.log(oneCall + $.param(oneCallParams));
+  return oneCall + $.param(oneCallParams);
 }
 
-//click functions
-// first call, city name gets coords
+// ----click function----
 $("#searchBtn").click(function (event) {
+  // prevent browser defaults on click
   event.preventDefault();
+
+  // ------add city name to city list using local storage, make link clickable------
+
   let getCoord = buildGetCoord();
-  console.log(getCoord);
+  // current weather api call to get lat and lon coordinates for user entered city
   $.ajax({
     url: getCoord,
     method: "GET"
-  }).then(function (response) {
+    //when current weather api call is done, log response and pull lat and lon to be used as parameters in next call
+  }).done(function (response) {
     console.log(response);
-  })
+    let lat = response.coord.lat;
+    let lon = response.coord.lon;
+    console.log(lat);
+    console.log(lon);
+    // insert lat and lon into parameters
+    let oneCall = buildOneCall(lat, lon);
+    // api call to one call api to get data to be displayed
+    $.ajax({
+      url: oneCall,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    })
 
-  // $(".form-control").val("");
+  });
+})
 
-});
-//use response to get lat and lon
+//localstorage set function for  city list, when clicked display current weather & forecast, move function above click function
+//localstorage get function to display city list, move function above click function
+//localstorage last search display on refresh, move function above click function
 
-//run api call with lat and lon to get current weather and display
-
-//run api call with lat and lon to get five day forecast and display
-
-//localstorage city list, when clicked display current weather & forecast
-//localstorage last search display on refresh
+// ----extras if finished early----
+//css styling
+//make input box clear when submitting
+//make it so submit can work on return button as well
